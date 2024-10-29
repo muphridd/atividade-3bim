f3b94d0f20d623102d5030623806ffb426c07973
blessyouboy <112110632+muphridd@users.noreply.github.com>
2024-10-29 09:28:25 -0300

    condicionais3bim-questao1.js

--- src/condicionais3bim-questao1.js
+++ src/condicionais3bim-questao1.js
@@ -1,27 +1,13 @@
-
-function compararNumeros(num1, num2) {
-  
-    const isNum1Impar = num1 % 2 !== 0;
-    const isNum2Impar = num2 % 2 !== 0;
-
-    if (isNum1Impar && !isNum2Impar) {
-        return `${num1} é maior (ímpar)`;
-    } else if (!isNum1Impar && isNum2Impar) {
-        return `${num2} é maior (ímpar)`;
+function comparaNumeros(num1, num2) {
+    if (num1 > num2) {
+        return `${num1} é maior que ${num2}`;
+    } else if (num2 > num1) {
+        return `${num2} é maior que ${num1}`;
     } else {
-      
-        if (num1 > num2) {
-            return `${num1} é maior`;
-        } else if (num2 > num1) {
-            return `${num2} é maior`;
-        } else {
-            return 'Os números são iguais';
-        }
+        return "Os números são iguais";
     }
 }
 
-const numero1 = 5; 
-const numero2 = 4;
-const resultado = compararNumeros(numero1, numero2);
-
-console.log(resultado);
+console.log(comparaNumeros(10, 5)); 
+console.log(comparaNumeros(3, 8));  
+console.log(comparaNumeros(7, 7));   
