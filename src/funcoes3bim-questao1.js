dd418a02a475ebc92d05c0a2eab2ab770cabe127
blessyouboy <112110632+muphridd@users.noreply.github.com>
2024-10-29 09:29:23 -0300

    funcoes3bim-questao1.js

--- src/funcoes3bim-questao1.js
+++ src/funcoes3bim-questao1.js
@@ -1,29 +1,7 @@
-function inverterNumero(numero) {
-    let numeroInvertido = '';
-    const strNumero = numero.toString();
-
-    for (let i = strNumero.length - 1; i >= 0; i--) {
-        const digito = strNumero[i];
-
-        switch (digito) {
-            case '0':
-            case '1':
-            case '2':
-            case '3':
-            case '4':
-            case '5':
-            case '6':
-            case '7':
-            case '8':
-            case '9':
-                numeroInvertido += digito;
-                break;
-            default:
-                break;
-        }
-    }
-    return Number(numeroInvertido);
+function inverteNumero(numero) {
+    const numeroInvertido = numero.toString().split('').reverse().join('');
+    return parseInt(numeroInvertido, 10);
 }
 
-
-console.log(inverterNumero(12356)); 
+console.log(inverteNumero(12356)); 
+console.log(inverteNumero(987654)); 
