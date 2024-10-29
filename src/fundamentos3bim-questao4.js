72b06f4abca6a94a5ce484b779f270caa70f34fc
blessyouboy <112110632+muphridd@users.noreply.github.com>
2024-10-29 09:26:22 -0300

    fundamentos3bim-questao4.js

--- src/fundamentos3bim-questao4.js
+++ src/fundamentos3bim-questao4.js
@@ -1,12 +1,8 @@
-
-function verificarNumeros(num1, num2) {
-    
-    return num1 === 30 || num2 === 30 || (num1 + num2 === 30) || (num1 === 12 && num2 === 12);
+function verificaNumero(num1, num2) {
+    return num1 === 30 || num2 === 30 || (num1 + num2 === 30);
 }
 
-
-const numero1 = 12; 
-const numero2 = 12;
-const resultado = verificarNumeros(numero1, numero2);
-
-console.log(`Os números ${numero1} e ${numero2} atendem às condições? ${resultado}`);
+console.log(verificaNumero(30, 5)); // true
+console.log(verificaNumero(15, 15)); // true
+console.log(verificaNumero(10, 20)); // true
+console.log(verificaNumero(10, 5)); // false
